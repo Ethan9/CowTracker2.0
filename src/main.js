d734d6286ad98cb60b06ea41641545e15af73cf6
Ethan Lea <ethanjlea@gmail.com>
2019-11-19 15:09:28 +0000

    Got login working

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: 'AIzaSyArwQyr27jLG9laV0FJskxa1zuubrXQxeo',
+  authDomain: 'cowtracker-5b405.firebaseapp.com',
+  databaseURL: 'https://cowtracker-5b405.firebaseio.com',
+  projectId: 'cowtracker-5b405',
+  storageBucket: 'cowtracker-5b405.appspot.com',
+  messagingSenderId: '656494149585',
+  appId: '1:656494149585:web:bf8406b22e857bb897eaf0',
+  measurementId: 'G-JFNHBR45PD'
+}
+
+firebase.initializeApp(config)
 
 new Vue({
   router,
