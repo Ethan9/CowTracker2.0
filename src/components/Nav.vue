1b386113e002f56ae4cc24abb6bd486435fed40f
Ethan Lea <ethan.lea@heroes.nl>
2020-02-10 08:07:01 +0000

    Fixed bootstrap

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    <nav>
-      <router-link class="spacing" v-for="routes in links"
-      v-bind:key="routes.id"
-      :to="`${routes.page}`">{{routes.text}}</router-link>
-    </nav>
+  <div class="navBar">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand @click="home">CowTracker</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav left>
+        <b-nav-item>
+        <router-link class="spacing" v-for="routes in links"
+        v-bind:key="routes.id"
+        :to="`${routes.page}`">{{routes.text}}</router-link>
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import firebase from 'firebase'
+
 export default {
   name: 'Navigation',
   data () {
@@ -21,20 +47,17 @@ export default {
         },
         {
           id: 1,
-          text: 'Log in',
-          page: '/login'
-        },
-        {
-          id: 2,
-          text: 'Sign Up',
-          page: '/sign-up'
-        },
-        {
-          id: 3,
           text: 'Cows',
           page: '/cow'
         }
       ]
+    }
+  },
+  methods: {
+    logout: function () {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
     }
   }
 }
@@ -43,5 +66,11 @@ export default {
 <style>
   .spacing {
     Margin-right: 10px;
+  }
+  b-navbar {
+    color: black;
+  }
+  a {
+    color: black;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="home">
-    <Navigation></Navigation>
+    <div class="bg-img">
+      <div class="container">
+        <Navigation></Navigation>
+      </div>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <button @click="logout">Logout</button>
     <router-view/>
@@ -27,3 +31,29 @@ export default {
   }
 }
 </script>
+
+<style>
+/* The hero image */
+.bg-img {
+  /* The image used */
+  background-image: url("/cow-header.jpg");
+
+  min-height: 380px;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  /* Needed to position the navbar */
+  position: relative;
+}
+
+/* Position the navbar container inside the image */
+.container {
+  position: absolute;
+  margin: 20px;
+  width: auto;
+}
+
+</style>
